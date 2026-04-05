a55770d6828b0fc2650aae3e460855bdcdd22f62
Aman Brahma <atanuaman25@gmail.com>
2026-04-05 21:34:43 +0530

    Create common.h with constants and function prototypes
    Add header file with constants and function declarations

--- common.h
+++ common.h
@@ -1,1 +1,14 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define PORT 8080
+#define SUBMIT 1
+#define REQUEST_TASK 2
+
+int recv_all(int sock, char *buffer, int size);
+
+int connect_to_server(const char *hostname, const char *port);
+
+#endif 
+
 
