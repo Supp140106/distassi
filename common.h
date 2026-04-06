27787be74763be8de844e0c250d2edff1997696a
Supprit Das <suppritdas@gmail.com>
2026-04-06 09:21:46 +0530

    Add comments and formatting to common.h

--- common.h
+++ common.h
@@ -1,3 +1,4 @@
+//begin Aman
 #ifndef COMMON_H
 #define COMMON_H
 
@@ -5,10 +6,12 @@
 #define SUBMIT 1
 #define REQUEST_TASK 2
 
+// Function to reliably receive `size` bytes from `sock` into `buffer`.
+// Returns 0 on success, or -1 on network failure/disconnection.
 int recv_all(int sock, char *buffer, int size);
 
+// Function to resolve DNS/IP and connect securely to the target
 int connect_to_server(const char *hostname, const char *port);
 
-#endif 
-
-
+#endif // COMMON_H
+//end Aman
